133f5442c4db9e135780cb2f13de080fbc37359a
nasir <evilleader91@gmail.com>
2026-04-12 20:53:34 +0200

    Finish data.py

--- src/health_app/data.py
+++ src/health_app/data.py
@@ -1,1 +1,61 @@
+import json
+from health_app.health import Health
+from collections import Counter
+
+
+def save(records):
+    data = []
+    for record in records:
+        item = {
+            "name": record.name,
+            "weight_kg": record.weight_kg,
+            "height_m": record.height_m,
+            "calculated_bmi": record.calculate_bmi()
+        }
+        data.append(item)
+
+    with open("health_records.json", "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def read(file):
+    try:
+        with open(file, "r") as f:
+            data = json.load(f)
+            people = []
+
+            for person in data:
+                record = Health(
+                    person["name"],
+                    person["weight_kg"],
+                    person["height_m"]
+                )
+                people.append(record)
+            return people
+    except FileNotFoundError:
+        return []
+
+def get_statistics(file):
+    records = read(file)
+    total_records = len(records)
+    bmi_cumulative = 0
+    categories = []
+
+    for person in records:
+        bmi_cumulative += person.calculate_bmi()
+        categories.append(person.category_bmi())
+
+    avg_bmi = round((bmi_cumulative / total_records), 2)
+    category_distribution = Counter(categories)
+    most_common_category = category_distribution.most_common(1)
+
+
+    return {
+        "total_records": total_records,
+        "avg_bmi": avg_bmi,
+        "most_common_category": most_common_category,
+        "category_distribution": category_distribution
+    }
+
+
 
